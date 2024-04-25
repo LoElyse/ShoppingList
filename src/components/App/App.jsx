51188774ed022d436c491df1f7e3287e062d0ed4
LoElyse <lora.elyse14@gmail.com>
2024-04-25 16:50:29 -0500

    added GET

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,39 +1,67 @@
 import React from 'react';
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Header from '../Header/Header.jsx';
 import './App.css';
 
-// USE state:
-
-
-
-
-// useEffect(() => {
-    
-// },[])
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <main>
-                <p>Under Construction...</p>
-            </main>
-        </div>
-    );
+
+
+// STATE api:
+
+const [items, setItems] = useState([]);
+
+//STATE input:
+
+const [itemInput, setItemInput] = useState('');
+const [quantityInput, setQuantityInput] = useState('');
+const [unitInput, setUnitInput] = useState('');
+
+
+
+// TODO: fetch the list of people from the server
+const getItems = () => {
+axios.get('/api/items')
+    .then((response) => {
+      console.log('We got famous peopple:', response.data);
+    // setItems(response.data);
+    })
+    .catch((error) => {
+      console.log('bummer error in get route:', error);
+    })
+}
+
+// useEffect(() => {
+// getItems()    
+// },[])
+
+return (
+    <div className="App">
+      <Header />
+      <main>
+        <h1>Add an Item</h1>
+        Input: <input type="text" />
+        Quantity: <input type="text" />
+        Unit: <input type="text" />
+      </main>
+      <body>
+        <h1>Shopping List</h1>
+        {/* <Header{itemInput.}/> */} 
+        <button>Reset</button>
+        <button>Clear</button>
+        <ul>
+            <li>
+                Apples
+                5 lbs
+            </li>
+        </ul>
+        <button>Buy</button>
+        <button>Remove</button>
+      </body>
+    </div>
+  );
 }
 
 export default App;
